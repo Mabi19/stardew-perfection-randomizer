9b905104ec7629bd36fe6f6f9d72be7355f05740
Mabi19 <mateuszbartnik33@gmail.com>
2023-10-17 21:33:24 +0200

    Redirect users without a profile
    (and vice versa)

---
+++ middleware/redirector.global.ts
@@ -0,0 +1,14 @@
+const nonProfileRequiredPages = new Set(["/"]);
+
+export default defineNuxtRouteMiddleware((to) => {
+    const hasSaveFile = Boolean(localStorage.getItem("currentProfile"));
+    if (nonProfileRequiredPages.has(to.path)) {
+        if (hasSaveFile) {
+            return navigateTo("/dashboard");
+        }
+    } else {
+        if (!hasSaveFile) {
+            return navigateTo("/");
+        }
+    }
+});
