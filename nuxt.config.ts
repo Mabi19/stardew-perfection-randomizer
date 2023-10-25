dea324bd2b80a2e466c9fb6cf105a254a205757f
Mabi19 <mateuszbartnik33@gmail.com>
2023-10-25 18:46:12 +0200

    Set base URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
     devtools: { enabled: true },
     ssr: false,
     app: {
+        baseURL: "/stardew-perfection-randomizer/",
         head: {
             link: [
                 { rel: "preconnect", href: "https://fonts.googleapis.com" },
