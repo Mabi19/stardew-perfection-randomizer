1dffaaf1cd0c69eda533261d43bec280984330fa
Mabi19 <mateuszbartnik33@gmail.com>
2023-11-03 22:52:32 +0100

    Add mirrored byte array utils

--- utils/bytearray.ts
+++ utils/bytearray.ts
@@ -8,12 +8,30 @@ namespace ByteArrayUtils {
         );
     }
 
+    export function decodeInteger(data: Uint8Array) {
+        let output = 0;
+        for (let i = data.length - 1; i >= 0; i--) {
+            const byte = data[i];
+            const shift = (data.length - i - 1) * 8;
+            output += byte << shift;
+        }
+        return output;
+    }
+
     export async function compressData(data: Uint8Array) {
         const stream = new CompressionStream("gzip");
         // write it to the stream...
         const piped = new Response(data).body!.pipeThrough(stream);
         // and read it back
         return await new Response(piped).blob();
+    }
+
+    export async function decompressData(data: Uint8Array) {
+        const stream = new DecompressionStream("gzip");
+        // write it to the stream...
+        const piped = new Response(data).body!.pipeThrough(stream);
+        // and read it back
+        return await new Response(piped).arrayBuffer();
     }
 }
 
