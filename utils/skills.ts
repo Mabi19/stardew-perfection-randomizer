01f241d4405c9c46683475ed208271297e26d2bf
Mabi19 <mateuszbartnik33@gmail.com>
2023-11-23 15:54:17 +0100

    Refactor skill XP values out

--- utils/skills.ts
+++ utils/skills.ts
@@ -1,8 +1,6 @@
-export const skills = [
-    "farming",
-    "mining",
-    "foraging",
-    "fishing",
-    "combat",
-] as const;
+export const skills = ["farming", "mining", "foraging", "fishing", "combat"] as const;
 export type Skill = (typeof skills)[number];
+
+export const skillXPValues = [
+    0, 100, 380, 770, 1300, 2150, 3300, 4800, 6900, 10000, 15000, 9999999,
+];
