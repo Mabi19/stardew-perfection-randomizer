446f005263974a8840b7e4f6fa7dc785964e61e3
Mabi19 <mateuszbartnik33@gmail.com>
2023-12-11 22:37:38 +0100

    Add particle settings to store

--- composables/settings-store.ts
+++ composables/settings-store.ts
@@ -1,12 +1,21 @@
 import { defineStore } from "pinia";
 
+export type PARTICLES_SETTINGS = "disabled" | "low" | "medium" | "high";
+
 export const useSettingsStore = defineStore("settings", () => {
     const selectedTheme = ref<string>(localStorage.getItem("settings:theme") ?? "system");
     watch(selectedTheme, () => {
         localStorage.setItem("settings:theme", selectedTheme.value);
     });
+    const selectedParticles = ref<PARTICLES_SETTINGS>(
+        (localStorage.getItem("settings:particles") as PARTICLES_SETTINGS) ?? "medium",
+    );
+    watch(selectedParticles, () => {
+        localStorage.setItem("settings:particles", selectedParticles.value);
+    });
 
     const systemIsDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+    const systemPrefersReducedMotion = useMediaQuery("(prefers-reduced-motion)");
 
     const theme = computed(() =>
         // This is a mess of ternaries:
@@ -19,8 +28,16 @@ export const useSettingsStore = defineStore("settings", () => {
             : selectedTheme.value,
     );
 
+    const particles = computed(() =>
+        systemPrefersReducedMotion ? "disabled" : selectedParticles.value,
+    );
+
     return {
         theme,
         selectedTheme,
+
+        particles,
+        selectedParticles,
+        systemPrefersReducedMotion,
     };
 });
