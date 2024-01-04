5d57b9569c30400406fbcc1953d01033b62568ea
Mabi19 <mateuszbartnik33@gmail.com>
2024-01-04 19:21:03 +0100

    Add goal info dialog

--- components/GoalList.vue
+++ components/GoalList.vue
@@ -4,13 +4,89 @@
             <tr>
                 <th>Comp.</th>
                 <th>Name</th>
+                <th><!-- settings button --></th>
             </tr>
         </thead>
         <tbody>
-            <ListGoal v-for="goal in filteredGoals" :goal="goal"></ListGoal>
+            <ListGoal
+                v-for="goal in filteredGoals"
+                :goal="goal"
+                @info-click="openGoalInfo"
+            ></ListGoal>
         </tbody>
     </table>
     <div v-else>No goals matching filter!</div>
+
+    <AppDialog
+        :open="infoGoal != null"
+        :title="infoGoal?.name ?? '<unknown>'"
+        @close="infoGoal = null"
+    >
+        <div class="goal-info-wrapper" v-if="infoGoal != null">
+            <div>
+                <strong>ID:</strong>
+                <code>{{ infoGoal.id }}</code>
+            </div>
+            <div>
+                <strong>Multiplicity:</strong>
+                <span>{{ infoGoal.multiplicity }}</span>
+            </div>
+            <div>
+                <strong>Image:</strong>
+                <a v-if="infoGoal.imageURL" :href="infoGoal.imageURL">{{ infoGoal.imageURL }}</a>
+                <span v-else>&lt;none&gt;</span>
+            </div>
+            <div>
+                <strong>Prerequisites:</strong>
+                <div class="prerequisites" v-if="infoGoalReqs">
+                    <span>
+                        {{ infoGoal.prerequisites.all ? "All" : "Any" }} of these goals must be met:
+                    </span>
+                    <ul>
+                        <li v-for="req in infoGoalReqs">
+                            <Goal
+                                v-if="store.goals[req.goal]"
+                                :goal="store.goals[req.goal]"
+                                class="inline"
+                            />
+                            <!-- this is required for tags to work -->
+                            <code v-else>{{ req.goal }}</code>
+                            <span
+                                v-if="req.multiplicity && req.multiplicity > 1"
+                                class="inline-goal-mult"
+                            >
+                                (x{{ req.multiplicity }})
+                            </span>
+                        </li>
+                    </ul>
+                </div>
+                <span v-else>&lt;none&gt;</span>
+            </div>
+            <div>
+                <strong>Implied XP:</strong>
+                <div class="implied-xp" v-if="!isEmpty(infoGoal.xp)">
+                    <ul>
+                        <li v-for="(amount, skill) in infoGoal.xp" class="capitalize">
+                            {{ amount }} {{ skill }}
+                        </li>
+                    </ul>
+                </div>
+                <span v-else>&lt;none&gt;</span>
+            </div>
+            <div class="button-box">
+                <AppButton
+                    icon="tune"
+                    :disabled="
+                        infoGoal.id == store.currentGoalID ||
+                        store.completion[infoGoal.id] >= infoGoal.multiplicity
+                    "
+                    @click="store.currentGoalID = infoGoal.id"
+                >
+                    Set as current
+                </AppButton>
+            </div>
+        </div>
+    </AppDialog>
 </template>
 
 <script setup lang="ts">
@@ -31,4 +107,52 @@ const filteredGoals = computed(() => {
         goal.name.toLowerCase().includes(lowerSearchTerm),
     );
 });
+
+const infoGoal = ref<Goal | null>(null);
+const infoGoalReqs = computed(() => {
+    if (infoGoal.value == null) return null;
+    return infoGoal.value.prerequisites.all ?? infoGoal.value.prerequisites.any ?? null;
+});
+
+function openGoalInfo(goal: Goal) {
+    infoGoal.value = goal;
+}
+
+function isEmpty(o: {}) {
+    return Object.keys(o).length == 0;
+}
 </script>
+
+<style scoped lang="scss">
+.goal-info-wrapper {
+    & > div {
+        & > strong {
+            margin-right: 0.5ch;
+        }
+    }
+}
+
+.button-box {
+    margin-top: 1rem;
+}
+
+.prerequisites,
+.implied-xp {
+    ul {
+        margin: 0;
+        padding-left: 2rem;
+    }
+}
+
+.inline {
+    display: inline;
+}
+
+.capitalize {
+    text-transform: capitalize;
+}
+
+.inline-goal-mult {
+    vertical-align: middle;
+}
+</style>
--- components/ListGoal.vue
+++ components/ListGoal.vue
@@ -4,11 +4,38 @@
         <td class="name">
             <label :for="`goal:${goal.id}`"><Goal :goal="goal" /></label>
         </td>
+        <td class="info">
+            <button class="info-button material-icons" @click="openDialog">more_horiz</button>
+        </td>
     </tr>
 </template>
 
 <script setup lang="ts">
 const props = defineProps<{
-    goal: Goal
+    goal: Goal;
 }>();
+
+const emit = defineEmits<{
+    infoClick: [goal: Goal];
+}>();
+
+// we want the list items to be as light as possible, so defer opening the dialog box to the parent
+function openDialog() {
+    emit("infoClick", props.goal);
+}
 </script>
+
+<style scoped lang="scss">
+.info-button {
+    padding: 0;
+    margin: 0;
+    border: none;
+    background: unset;
+    cursor: pointer;
+    font-size: 1.25rem;
+
+    &:hover {
+        opacity: 0.7;
+    }
+}
+</style>
