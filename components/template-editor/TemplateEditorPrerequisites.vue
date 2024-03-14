5a00d84ed1ab1daf5308fdcd9fd30f488a76a329
Mabi19 <mateuszbartnik33@gmail.com>
2024-03-14 20:15:02 +0100

    Pass updates through the prerequisite tree

--- components/template-editor/TemplateEditorPrerequisites.vue
+++ components/template-editor/TemplateEditorPrerequisites.vue
@@ -2,19 +2,23 @@
     <div class="prereq-header">
         <span>{{ type }} of:</span>
         <div class="header-buttons">
-            <PlainIconButton icon="create_new_folder" title="Create prerequisite group" />
-            <PlainIconButton icon="add" title="Create prerequisite" />
+            <PlainIconButton
+                icon="create_new_folder"
+                title="Create prerequisite group"
+                @click="createFolder"
+            />
+            <PlainIconButton icon="add" title="Create prerequisite" @click="createPrerequisite" />
         </div>
     </div>
     <ul class="prereq-list">
-        <li v-for="req in value.all ?? value.any ?? []">
+        <li v-for="(req, idx) in value.all ?? value.any ?? []">
             <template v-if="'goal' in req"
                 >{{ req.goal }}
                 <template v-if="!req.goal.startsWith('#')"
                     >x{{ req.multiplicity ?? 1 }}</template
                 ></template
             >
-            <TemplateEditorPrerequisites :value="req" v-else />
+            <TemplateEditorPrerequisites :value="req" v-else @update="passUpdate(idx, $event)" />
         </li>
     </ul>
 </template>
@@ -24,6 +28,10 @@ const props = defineProps<{
     value: PrerequisiteGroup;
 }>();
 
+const emit = defineEmits<{
+    update: [data: PrerequisiteGroup];
+}>();
+
 const type = computed(() => {
     if ("all" in props.value) {
         return "all";
@@ -31,6 +39,44 @@ const type = computed(() => {
         return "any";
     }
 });
+
+// Modify the data in-place, then pass it up
+function passUpdate(idx: number, newData: Prerequisite) {
+    // get the list of children
+    let workCopy: Prerequisite[];
+    if (props.value.all) {
+        workCopy = props.value.all;
+    } else if (props.value.any) {
+        workCopy = props.value.any;
+    } else {
+        return;
+    }
+
+    // clone
+    workCopy = JSON.parse(JSON.stringify(workCopy));
+
+    // swap out the data
+    workCopy.splice(idx, 1, newData);
+
+    // wrap it again
+    if (props.value.all) {
+        emit("update", { all: workCopy });
+    } else if (props.value.any) {
+        emit("update", { any: workCopy });
+    }
+}
+
+function createFolder() {
+    const newData = JSON.parse(JSON.stringify(props.value));
+    if (newData.all) {
+        newData.all.push({ all: [] });
+    } else if (newData.any) {
+        newData.any.push({ all: [] });
+    }
+    emit("update", newData);
+}
+
+function createPrerequisite() {}
 </script>
 
 <style scoped lang="scss">
