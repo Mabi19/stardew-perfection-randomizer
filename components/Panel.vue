cba7613b9fe4dbcf945681afb87bf7f70195f268
Mabi19 <mateuszbartnik33@gmail.com>
2024-03-18 20:42:00 +0100

    Add Panel component

---
+++ components/Panel.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="panel" :class="[type]"><slot /></div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    type: "warning";
+}>();
+</script>
+
+<style scoped lang="scss">
+@use "sass:color";
+
+@mixin panel-color($base) {
+    border: 2px solid $base;
+    background-color: color.change($base, $alpha: 0.25);
+}
+
+.panel {
+    padding: 0.5rem;
+    border-radius: 0.5rem;
+}
+
+.warning {
+    @include panel-color(#ffda0b);
+}
+</style>
