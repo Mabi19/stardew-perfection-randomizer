4378cf3fdd010462df64b3ee4854cba3a696b862
Mabi19 <mateuszbartnik33@gmail.com>
2024-04-30 23:13:38 +0200

    Fix tests
    Tests don't have fetch, so instead read the files directly

--- utils/validate.test.ts
+++ utils/validate.test.ts
@@ -1,11 +1,16 @@
 import { expect, test } from "vitest";
 import { validateTemplate } from "./validate";
-import { getPredefinedTemplate } from "./goals";
+import { readFile } from "node:fs/promises";
+
+async function getTemplateFromFile(path: string) {
+    const templateString = await readFile(`./templates/${path}`, { encoding: "utf-8" });
+    return JSON.parse(templateString);
+}
 
 test("standard.json validates", async () => {
-    expect(validateTemplate(await getPredefinedTemplate("standard")!)).toBe(true);
+    expect(validateTemplate(await getTemplateFromFile("standard.json"))).toBe(true);
 });
 
 test("hardcore.json validates", async () => {
-    expect(validateTemplate(await getPredefinedTemplate("hardcore")!)).toBe(true);
+    expect(validateTemplate(await getTemplateFromFile("hardcore.json"))).toBe(true);
 });
