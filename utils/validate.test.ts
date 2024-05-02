28f1962f4f9f905e036558c2a8f7feda3ad0fbe1
Mabi19 <mateuszbartnik33@gmail.com>
2024-05-02 22:31:58 +0200

    Add new templatest to tests

--- utils/validate.test.ts
+++ utils/validate.test.ts
@@ -11,6 +11,14 @@ test("standard.json validates", async () => {
     expect(validateTemplate(await getTemplateFromFile("standard.json"))).toBe(true);
 });
 
+test("standard_1_6.json validates", async () => {
+    expect(validateTemplate(await getTemplateFromFile("standard_1_6.json"))).toBe(true);
+});
+
 test("hardcore.json validates", async () => {
     expect(validateTemplate(await getTemplateFromFile("hardcore.json"))).toBe(true);
+});
+
+test("hardcore_1_6.json validates", async () => {
+    expect(validateTemplate(await getTemplateFromFile("hardcore_1_6.json"))).toBe(true);
 });
