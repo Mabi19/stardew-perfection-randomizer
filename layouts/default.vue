1a896057da864e1e601eacbd25cb6dd02fd509aa
Mabi19 <mateuszbartnik33@gmail.com>
2024-05-28 20:58:37 +0200

    Remove RC badge

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,6 @@
         <NavBar>
             <img src="~/assets/logo.webp" alt="Randomizer logo" />
             <span class="app-name">Stardew Perfection Randomizer</span>
-            <span class="badge green small-hide">RC</span>
             <div class="divider"></div>
             <NuxtLink to="/dashboard"
                 ><span class="material-icons">flag</span><span>Dashboard</span></NuxtLink
